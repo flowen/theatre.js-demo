43b8dc2d56a5aa4e34aa4fbad057f5ced30ca210
fLowen <lowen@flowen.nl>
2019-05-03 15:27:04 +0200

    modified question into sentence and retrieve name from data-attr

--- main-project/src/theatre-timelines/scene-chorus.js
+++ main-project/src/theatre-timelines/scene-chorus.js
@@ -4,25 +4,29 @@ import Charming from '../utils/charming'
 const sceneChorus = document.querySelector('.scene--chorus')
 
 const sceneQuestions = Array.from(sceneChorus.querySelectorAll(`.intro, .question`))
-sceneQuestions.forEach((question, i) => {
-  Charming(question, {
+sceneQuestions.forEach((sentence, i) => {
+  Charming(sentence, {
     splitRegex: ' ', // .split(' ') but we a space back in with css
     classPrefix: 'word',
   })
 
-  const sceneWords = Array.from(question.querySelectorAll(`.word`))
+  const sceneWords = Array.from(sentence.querySelectorAll(`.word`))
 
   sceneWords.forEach((obj, j) => {
-    sceneWords[j] = timeline.getObject(`Chorus - Q${i + 1} - ${obj.dataset.objName}`, obj, {
-      props: {
-        opacity: { type: 'number' },
-        skewX: { type: 'number' },
-        skewY: { type: 'number' },
-        scaleX: { type: 'number' },
-        scaleY: { type: 'number' },
-        scaleZ: { type: 'number' },
-      },
-    })
+    sceneWords[j] = timeline.getObject(
+      `Chorus - ${sentence.dataset.objName} - ${obj.dataset.objName}`,
+      obj,
+      {
+        props: {
+          opacity: { type: 'number' },
+          skewX: { type: 'number' },
+          skewY: { type: 'number' },
+          scaleX: { type: 'number' },
+          scaleY: { type: 'number' },
+          scaleZ: { type: 'number' },
+        },
+      }
+    )
 
     sceneWords[j].onValuesChange(props => {
       const css = `opacity: ${props.opacity};
